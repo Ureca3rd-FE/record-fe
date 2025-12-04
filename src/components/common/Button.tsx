0596a9504793e41d3bdf1c0bc7e36dc4882dc561
Zoo2-bi <bagjui068@gmail.com>
2025-12-04 15:40:11 +0900

    fix: style 선언 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { cn } from "@/utils/cn";
 
 export default function Button({
@@ -11,10 +12,9 @@ export default function Button({
     <button
       type={type}
       className={cn(
-  "w-full bg-[#8A3F2E] text-white font-semibold px-5 py-3 rounded-[20px] transition active:scale-[0.98]",
-  className
-)}
-
+        "w-full bg-[var(--color-primary-200)] text-white font-semibold px-5 py-3 rounded-[20px] transition active:scale-[0.98]",
+        className
+      )}
       {...props}
     >
       {children}
