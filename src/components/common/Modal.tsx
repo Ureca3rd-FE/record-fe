94445a41114e68085b5d5ebd68151b4b2bd1fb19
Zoo2-bi <bagjui068@gmail.com>
2025-12-05 10:54:28 +0900

    fix: onConfirm추가하였음 버튼없는 애들도 showButton으로 처리

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -13,6 +13,8 @@ export interface ModalProps {
   description?: string;
   buttonText?: string;
   children?: React.ReactNode;
+  showButton?: boolean; //버튼 없는 애들
+  onConfirm?: () => void;
 }
 
 
@@ -22,28 +24,42 @@ export default function Modal({
   title = "알림",
   description,
   buttonText = "확인",
+  onConfirm,
+  showButton = true,
   children,
 }: ModalProps) {
+
   if (!open) return null;
+
+  //확인 버튼 클릭 시 실행될 함수! *광수오빠가 말한 onClick Props가 이거 맞나요? 버튼 클릭시 원하는 로직 실행하기
+  const handleClick = () => {
+    if(onConfirm) onConfirm();
+    else onClose();
+  };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       
+      {/* 배경 클릭하면 모달 맏힘  */}
       <div
         className="absolute inset-0 bg-black/30 backdrop-blur-sm"
         onClick={onClose}
       ></div>
 
+
       {/* 박스x */}
       <div className="relative z-10 bg-secondary-100 w-[90%] rounded-2xl p-8 shadow-lg text-center">
-
+        {/* 중요! 버튼이 있을 시 = true 일때만 닫기 아이콘을 표시 */}
         {/* 닫기 */}
+        {showButton && (
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-primary-200 text-2xl"
         >
         <CancelIcon className="w-6 h-6 text-primary-200" />
         </button>
+        )}
+
 
         {/* 제목 */}
         <h2 className="text-primary-200 font-bold text-2xl mb-4">
@@ -59,8 +75,10 @@ export default function Modal({
           </p>
         )}
 
-        {/* 버튼 */}
+        {/* 버튼이 보일때 =true일때만 버튼 표시 */}
+        {showButton && (
         <Button onClick={onClose}>{buttonText}</Button>
+        )}
       </div>
     </div>
   );
