7399e50e3badcff0108c17855a6cfda226596d0e
gs0428 <lider28@naver.com>
2025-12-09 09:42:06 +0900

    feat: 서비스 첫 화면 페이지 퍼블리싱

--- src/app/(withoutNavbar)/page.tsx
+++ src/app/(withoutNavbar)/page.tsx
@@ -1,3 +1,13 @@
+import StartButtons from "@/components/onboarding/start-buttons";
+
 export default function Onboarding() {
-  return <div>Onboarding</div>;
+  return (
+    <main className="flex min-h-screen flex-col px-4 py-6">
+      <div className="font-pretendard flex flex-1 flex-col justify-center space-y-7 text-center font-semibold">
+        <h1 className="font-pretendard text-4xl font-semibold">re:cord</h1>
+        <h2 className="text-primary-100">당신의 이야기를 기록해요</h2>
+      </div>
+      <StartButtons />
+    </main>
+  );
 }
---
+++ src/components/onboarding/start-buttons.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import GoogleIcon from "@/assets/google.svg";
+import Button from "@/components/common/Button";
+
+export default function StartButtons() {
+  const router = useRouter();
+
+  const onGoogleLogin = () => {
+    // TODO: Google 로그인 API 연결
+    // 백엔드 base url + /oauth2/authorization/google로 router.push 하기
+  };
+
+  return (
+    <div className="space-y-4 text-sm">
+      <Button
+        onClick={onGoogleLogin}
+        className="flex items-center justify-center gap-2 rounded-xl bg-white py-4 text-black"
+      >
+        <GoogleIcon />
+        <span>Google 계정으로 로그인</span>
+      </Button>
+      <Button onClick={() => router.push("/home")} className="rounded-xl py-4">
+        로그인 없이 둘러보기
+      </Button>
+    </div>
+  );
+}
