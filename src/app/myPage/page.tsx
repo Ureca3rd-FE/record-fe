db18cf790ec5bbcdb021feeb75a842295c198152
Zoo2-bi <bagjui068@gmail.com>
2025-12-12 09:26:15 +0900

    fix: 파일 이름 수정

--- src/app/myPage/page.tsx
+++ src/app/myPage/page.tsx
@@ -14,12 +14,11 @@ import { useMyInfo } from "@/hooks/useMyInfo";
 
 export default function MyPage() {
   const { data, isLoading, error } = useMyInfo();
-
   return (
     <div className="min-h-screen bg-[#f3e5d0] pb-28">
-      {/* 상단 갈색 섹션 */}
+      {/* 상단 갈색  */}
       <div className="from-primary-200 rounded-b-[40px] bg-linear-[225deg] to-[#B79182] px-6 pt-10 pb-10 text-white shadow-md">
-        {/* 로고 — left top */}
+        {/* 로고 */}
         <div className="mb-6">
           <Logo className="h-auto w-20" />
         </div>
@@ -28,10 +27,10 @@ export default function MyPage() {
         <div className="mb-6 flex items-center justify-between rounded-2xl bg-white/20 p-4 shadow backdrop-blur-sm">
           <div className="flex flex-col">
             <p className="text-lg font-bold text-white">
-              {isLoading ? "로딩중..." : data?.nickname}
+              {isLoading ? "로딩 중 ... " : data?.nickname}
             </p>
             <p className="mt-1 rounded bg-white/20 px-2 py-0.5 text-sm text-white">
-              {isLoading ? "로딩중..." : data?.email}
+              {isLoading ? "로딩 중 ... " : data?.email}
             </p>
           </div>
           <div className="relative flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-white">
