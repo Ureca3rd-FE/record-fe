4b7a6f3c1f0094060a536b788874a7223439eab9
gs0428 <lider28@naver.com>
2025-12-15 13:49:22 +0900

    feat: 오늘의 질문 조회 API 연동

--- src/app/(withNavbar)/home/page.tsx
+++ src/app/(withNavbar)/home/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 
 import Logo from "@/assets/logo.svg";
 import SubmitDiary from "@/components/home/SubmitDiary";
+import { getTodayQuestion } from "@/services/question";
+
+import { format } from "date-fns";
 
 const curiousDalbam = "/dalbam/curious.webp";
 
-export default function Home() {
+const today = format(new Date(), "yyyy-MM-dd");
+
+export default async function Home() {
+  const { result } = await getTodayQuestion(today);
+
   return (
     <main className="px-4 pt-6 pb-[calc(var(--spacing-navbar)+24px)]">
       <header>
@@ -23,8 +30,7 @@ export default function Home() {
           <p>오늘의 달밤 일기</p>
           <Image src={curiousDalbam} alt="curious dalbam" width={120} height={120} />
         </div>
-        {/* TODO: 오늘의 일기 API 연동하여 표시하기 */}
-        <p className="mt-5 text-center">오늘 하루 중 가장 기분 좋았던 순간은 언제였나요?</p>
+        <p className="mt-5 text-center">{result.question}</p>
       </div>
       <SubmitDiary />
     </main>
